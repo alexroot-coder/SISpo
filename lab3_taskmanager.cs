88595d3c2e14d0b1011860a36d5399f2ed6cc423
alexroot-coder <60644048+alexroot-coder@users.noreply.github.com>
2021-10-26 08:47:10 +0700

    Update lab3_taskmanager.cs

--- lab3_taskmanager.cs
+++ lab3_taskmanager.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using static System.Threading.Thread;
 
@@ -8,9 +10,17 @@ namespace taskmngr
     internal class Program
     {   
         public static Process[] list_of_processes = Process.GetProcesses();
+        public static List<Process> tttt = list_of_processes.ToList();
         public static int iter = 0;
         public static int total_processes_length = list_of_processes.Length;
-        
+        public static bool sorted1 = false;
+        public static bool sorted2 = false;
+        public static bool sorted3 = false;
+        private static DateTime lastTime;
+        private static TimeSpan lastTotalProcessorTime;
+        private static DateTime curTime;
+        private static TimeSpan curTotalProcessorTime;
+
         static int Compare(int a, int b)
         {
             if(a > b)
@@ -27,13 +37,14 @@ namespace taskmngr
             }
             return 0;
         }
-        
+
         public static void Main(string[] args)
         {
             Console.Title = "TaskManager";
-            Console.SetWindowSize(120, 30);
+            Console.SetWindowSize(120, 31);
             var first_thrd = new Thread(first_output);
             first_thrd.Start();
+            
             while (true)
             {
                 ConsoleKey key = Console.ReadKey(true).Key;
@@ -56,16 +67,63 @@ namespace taskmngr
                         iter = total_processes_length-28;
                     }
                 }
-                if(key == ConsoleKey.Escape)
+
+                if (key == ConsoleKey.A) 
                 {
+                    sorted1 = true;
+                    sorted2 = false;
+                    sorted3 = false;
+                }
+                
+                if (key == ConsoleKey.S)
+                {
+                    sorted1 = false;
+                    sorted2 = true;
+                    sorted3 = false;
+                }
+                if (key == ConsoleKey.D)
+                {
+                    sorted1 = false;
+                    sorted2 = false;
+                    sorted3 = true;
+                }
+                
+                if(key == ConsoleKey.Escape)
+                {   
                     first_thrd.Abort();
                     break;
                 }
-
             }
-
         }
 
+        //     ДОЛЖНО СЧИТАТЬ CPU_USAGE про его имени
+        // static string CPU_USAGE(string processName)
+        // {
+        //     var result_cpu_usage = "";
+        //     Process[] pp = Process.GetProcessesByName(processName);
+        //     Process p = pp[0];
+        //     if (lastTime == null || lastTime == new DateTime())
+        //     {
+        //         lastTime = DateTime.Now;
+        //         lastTotalProcessorTime = p.TotalProcessorTime;
+        //     }
+        //     else
+        //     {
+        //         curTime = DateTime.Now;
+        //         curTotalProcessorTime = p.TotalProcessorTime;
+        //
+        //         double CPUUsage = (curTotalProcessorTime.TotalMilliseconds - lastTotalProcessorTime.TotalMilliseconds) / curTime.Subtract(lastTime).TotalMilliseconds / Convert.ToDouble(Environment.ProcessorCount);
+        //         //Console.WriteLine("{0} CPU: {1:0.0}%",processName,CPUUsage * 100);
+        //         lastTime = curTime;
+        //         lastTotalProcessorTime = curTotalProcessorTime;
+        //         result_cpu_usage = String.Format("CPU: {0:0.0}%",CPUUsage * 100);
+        //         return result_cpu_usage;
+        //     }
+        //     
+        //     return result_cpu_usage;
+        // }
+        //
+        
         static void first_output()
         {
             while (true)
@@ -73,16 +131,40 @@ namespace taskmngr
                 Console.CursorVisible = false;
                 Console.SetCursorPosition(0,0);
                 //Console.Clear();
-                Console.WriteLine("{0,-5}   {1,-35} {2,-25}  {3,-7}      {4,-7}","№", "Name", "Ram" , "PID","CPU");
-                list_of_processes = Process.GetProcesses();
-                total_processes_length = list_of_processes.Length;
+                Console.WriteLine("{0,-5}   {1,-35} {2,-5} \t {3,-7}      {4,-20}","№", "Name", "Ram" , "PID","CPU");
                 Console.WriteLine("-------------------------------------------------------------------------------------------");
- 
+                list_of_processes = Process.GetProcesses();
+                tttt = list_of_processes.ToList();
+                total_processes_length = tttt.Count;
+                
+                if (sorted1 == true)
+                {
+                    tttt = tttt.OrderBy(process => process.ProcessName).ToList();
+                }
+
+                if (sorted2 == true)
+                {
+                    tttt = tttt.OrderBy(process => process.PrivateMemorySize64).ToList();
+                }
+                
+                if (sorted3 == true)
+                {
+                    tttt = tttt.OrderBy(process => process.Id).ToList();
+                }
+                
                 for (int i = iter; i < Compare(iter + 28 , total_processes_length); i++)
                 {
-                    Console.WriteLine("{0,-5}   {1,-35} {2,-25}  {3,-7}  ",i, list_of_processes[i].ProcessName, list_of_processes[i].PrivateMemorySize64 , list_of_processes[i].Id );
+                    using (PerformanceCounter pcProcess = new PerformanceCounter("Process", "% Processor Time", tttt[i].ProcessName)) {
+                        pcProcess.NextValue();
+                            //System.Threading.Thread.Sleep(500);
+                        //Console.WriteLine();    
+                        //string v = string.Format("Process:{0} CPU% {1}", tttt[i].ProcessName, );
+                        Console.WriteLine("{0,-5}   {1,-35} {2,-5}M \t {3,-7} \t {4,7}% ", i, tttt[i].ProcessName, tttt[i].PrivateMemorySize64 / 1000000L, tttt[i].Id, pcProcess.NextValue() / 10 );
+                    }
+                    
                 }
-                //Sleep(650);
+                Thread.Sleep(100);
+                //Sleep(50);
             }
         }
     }
